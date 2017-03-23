8d0703624811191441051f3552a58d306288adff
Evan DiGiambattista <edigiam@me.com>
2017-03-23 12:03:53 -0400

    Set up Firebase in App.vue

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
+import VueFire from 'vuefire';
 import App from './App';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
-
+Vue.use(VueFire);
 Vue.use(VueMaterial);
 /* eslint-disable no-new */
 new Vue({
