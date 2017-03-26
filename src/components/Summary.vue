c1ab140d6bd264b7c29001ec6accf34c54c7dd3e
Evan DiGiambattista <edigiam@me.com>
2017-03-25 23:43:10 -0400

    Testing conditional component

---
+++ src/components/Summary.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h1>Test</h1>
+		<p>{{ test() }}</p>
+	</div>
+</template>
+
+<script>
+import { getDate, formatDate } from '../util';
+import db from '../db';
+export default {
+  name: 'Summary',
+  firebase: {
+  	punches: db.ref('punches')
+  },
+  methods: {
+  	test: function() {
+  		console.log(this.$store.state.punches)
+  	}
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
